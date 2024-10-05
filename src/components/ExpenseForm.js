2f6bf4fc716ccd0436029f515dc542ce09078fa3
gagandayal829ca <gagandayal829ca@gmail.com>
2024-10-05 11:29:05 -0400

    new code

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExpenseForm = () => {
+  return (
+    <div>
+      <h2>Expense Form</h2>
+    </div>
+  );
+};
+
+export default ExpenseForm;
---
+++ src/pages/expenseForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ExpenseForm from "../components/ExpenseForm";
+
+const expenseForm = () => {
+  return <ExpenseForm />;
+};
